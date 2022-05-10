6d939abc8fdf7389169167bda561437bafbfa1db
hsrsec <hsrsec@gmail.com>
2022-05-10 21:37:16 +0200

    process_data.py: added filter for binary categories

--- data/process_data.py
+++ data/process_data.py
@@ -70,7 +70,7 @@ class DataPipeline_DisasterResponse():
             categories[column] = pd.to_numeric(categories[column])
         
         # Elimination of those rows which are multiclass and not binary
-        categories.drop(categories[categories.related == 2].index)
+        categories.drop(categories[categories.related == 2].index, inplace=True)
 
         # 3. Step
         self.df = self.df.drop('categories', axis=1)
